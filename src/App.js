cdaf2b86931301a98d960c06808060a89bb7b6d3
eric-nguyen <nguyen.cong.thai@overdose.digital>
2022-10-18 17:38:58 +0700

    create new module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./styles.css";
-
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h3>Đây là danh sách công việc cần hoàn thành</h3>
+    </>
   );
 }
+
+export default App;
